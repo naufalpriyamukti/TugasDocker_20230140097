0eb707b29534947b20d12031feb475d40b4c34d8
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2026-04-13 03:01:01 +0700

    first commit

---
+++ praktikum6/src/test/java/com/example/praktikum6/Praktikum6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.praktikum6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Praktikum6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
