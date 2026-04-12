7e8c5608b797be1b70fdbc631dccf50ef15b4eca
naufalpriymukti <n.priyamukti.est2004@gmail.com>
2026-04-13 03:07:14 +0700

    tambahkan library

---
+++ praktikum6/src/main/java/com/example/praktikum6/controller/UserController.java
@@ -0,0 +1,9 @@
+package com.example.praktikum6.controller;
+
+import com.example.praktikum6.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
